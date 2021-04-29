0bd6618c397fa41e9472db26283aeb96d1e34b92
florchao <58371281+florchao@users.noreply.github.com>
2021-04-29 18:19:58 -0300

    Agrego el texto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
+  <div class ="menu" style="margin-top: 40px; margin-left: 2%" >
   <v-img
-      height="300" plain
-      style="margin-top: 70px; margin-left: 5%"
-      width="500">
+    height="300"
+    plain
+    width="500">
     <v-img
         height="300"
         width="500"
         src="@/assets/LogoAzul.png"
     ></v-img>
   </v-img>
+  <h3>¡Dejanos a nosotros armarte la rutina!</h3>
+  <h3>Empeza a entrenar hoy con rutinas pensadas</h3>
+    <h3>para vos… y sin cargo  </h3>
+  </div>
 </template>
 
 <script>
