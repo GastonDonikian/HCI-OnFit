67fded2d6bbfdb3f7e2e02487f045dc49e9f8ce0
florchao <fchao@itba.edu.ar>
2021-04-30 10:51:31 -0300

    se mejora el estilo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,24 @@
     height="300"
     plain
     style="margin-top: 3%"
-    width="500">
-    <v-img
-        height="300"
-        width="500"
-        src="@/assets/LogoAzul.png"
-    ></v-img>
-  </v-img>
-  <h3 style=" background-color: #EBEBEB; opacity: 0.7; display: inline-block; padding-left: 10px; margin-left: 5%;border-radius: 5px"  class="black--text">¡Dejanos a nosotros armarte la rutina! <br>
+    src="@/assets/LogoAzul.png"
+    width="500"/>
+  <h3 class="backText black--text">¡Dejanos a nosotros armarte la rutina! <br>
   Empeza a entrenar hoy con rutinas <br>
-  pensadas para vos… y sin cargo  </h3>
+  pensadas para vos… y sin cargo</h3>
   </div>
 </template>
 
+<style scoped>
+
+.backText {
+  background-color: #EBEBEB;
+  opacity: 0.7;
+  display: inline-block;
+  padding-left: 10px;
+  margin-left: 5%;
+  border-radius: 5px;
+}
+</style>
 <script>
 </script>
