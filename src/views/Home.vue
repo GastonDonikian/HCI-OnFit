cab30a9696caa47c5dd3411d56c5307823182a08
florchao <fchao@itba.edu.ar>
2021-04-30 10:04:52 -0300

    cambio de logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,9 @@
         src="@/assets/LogoAzul.png"
     ></v-img>
   </v-img>
-  <h3>¡Dejanos a nosotros armarte la rutina!</h3>
-  <h3>Empeza a entrenar hoy con rutinas</h3>
-  <h3>pensadas para vos… y sin cargo  </h3>
+  <h3 style=" background-color: #EBEBEB; opacity: 0.7; display: inline-block; padding-left: 10px; margin-left: 5%;border-radius: 5px"  class="black--text">¡Dejanos a nosotros armarte la rutina! <br>
+  Empeza a entrenar hoy con rutinas <br>
+  pensadas para vos… y sin cargo  </h3>
   </div>
 </template>
 
