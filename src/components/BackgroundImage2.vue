a69402d37cbb2f4194225bb17576af5ab3d8417b
florchao <fchao@itba.edu.ar>
2021-04-30 09:20:11 -0300

    cambio de logo

---
+++ src/components/BackgroundImage2.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <span class="imgBack"></span>
+</template>
+
+<script>
+export default {
+  name: "BackgroundImage2.vue"
+}
+</script>
+
+<style scoped>
+.imgBack {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  margin: 0 0 0 0;
+  left: 0;
+  z-index: 0;
+  background: url('../assets/backgroundImage2.png') no-repeat center center fixed;
+  background-size: cover;
+}
+</style>
