0e57a2a33ae806978c00d960e9b2039ddbba8493
florchao <fchao@itba.edu.ar>
2021-05-05 15:33:01 -0300

    No me redirecciona al view Rutine porque no me lo exporta. No se porque

---
+++ src/components/ViewRutine.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="margin-left: 20px; margin-top: 20px">
+    <v-card width="600px" min-height="500px" style="color: #EBEBEB; padding-bottom: 20px">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1 black--text"> TITULO </v-list-item-title>
+          <v-list-item-subtitle> SUBTITULO 1
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewRutine"
+}
+</script>
+export default()
+<style scoped>
+
+</style>
