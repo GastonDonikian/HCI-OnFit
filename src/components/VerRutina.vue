699deda579708cc56cca5767bfea44336508ce23
arfernandez2000 <arfernandez@itba.ed.ar>
2021-05-09 18:39:35 -0300

    ver rutina

--- src/components/VerRutina.vue
+++ src/components/VerRutina.vue
@@ -1,25 +1,70 @@
 <template>
-  <div align="center">
-    <v-card width="500px" color="#EBEBEB"  align="center" class="principalRutina">
+  <div align="center" @loadstart="loadRoutine">
+    <v-card width="500px" color="#EBEBEB" class="principalRutina">
       <v-col style="padding-left: 5%; padding-top: 5%; padding-bottom: 5%">
-      <v-row>
-      <h2>NOMBRE</h2>
-      </v-row>
-      <v-row>
-      <h3>Categoria</h3>
-      </v-row>
-      <v-row>
-      <s1>Descripcion</s1>
+        <v-row>
+          <h2>NOMBRE</h2>
+        </v-row>
+        <v-row>
+          <h3>Categoria</h3>
+        </v-row>
+        <v-row>
+          <h3>Descripcion</h3>
+        </v-row>
+        <v-row>
+          <v-card
+              elevation="0"
+              color="#EBEBEB"
+              outlined
+              style="border-color: #E46271"
+              class="text-left"
+          >
+            <h4>Entrada en calor</h4>
+            <v-banner v-for="ex in this.entrada" :key="ex.id">
+              <v-row style="padding: 5% 5% 5% 5%">
+                <v-col>
+                  <v-row class="text--black"><h4>{{ ex.titulo }}</h4></v-row>
+                  <v-row class="text--darken-4">{{ex.description}}</v-row>
+                </v-col>
 
-      </v-row>
+                <v-spacer></v-spacer>
+                <v-col v-if="ex.repetitions != null">x{{ex.repetitions}}</v-col>
+                <v-col v-if="ex.duration != null">{{ex.duration}}s</v-col>
+              </v-row>
+            </v-banner>
+            <h4>Principal</h4>
+            <h4>Elongación</h4>
+          </v-card>
+        </v-row>
       </v-col>
     </v-card>
   </div>
 </template>
 
 <script>
+import RoutineStore from "../store/RoutineStore";
+
 export default {
-  name: "VerRutina"
+  name: "VerRutina",
+  data() {
+    return{
+      store: RoutineStore,
+      elong: [],
+      entrada: [],
+      prin: []
+    }
+  },
+  methods: {
+    loadRoutine(){
+      let routine = this.store.get(this.store.currentRoutine)
+      this.entrada = routine.entradaEnCalor;
+      this.prin = routine.principal;
+      this.elong = routine.entradaEnCalor
+    }
+  },
+  created(){
+    this.loadRoutine();
+  }
 }
 </script>
 
