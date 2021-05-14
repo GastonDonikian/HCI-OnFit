1efd2b0d1e913b866ba4ea7ff320b1134b65bde5
hfinucci <hfinucci@itba.edu.ar>
2021-05-14 20:31:59 -0300

    Commiteo por si las moscas

--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -2,7 +2,7 @@
   <div >
     <v-row justify="center">
       <v-spacer></v-spacer>
-      <v-col>
+<!--      <v-col>-->
         <v-card
             v-if="this.fav_length=== 0"
             color="#EBEBEB"
@@ -51,7 +51,7 @@
               </v-col>
             </v-row>
           </v-card>
-      </v-col>
+<!--      </v-col>-->
       <v-spacer></v-spacer>
     </v-row>
   </div>
